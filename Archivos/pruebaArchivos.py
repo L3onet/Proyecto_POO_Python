72fc51a6cb178a30ed1c3bd402736d9ebaf47c2c
Leonel Gonzalez Vidales <leonel@outlook.com>
2022-05-31 08:10:47 -0500

    Lectura de datos desde un archivo de texto

--- Archivos/pruebaArchivos.py
+++ Archivos/pruebaArchivos.py
@@ -1,13 +1,40 @@
+from abc import ABCMeta
+
+class ReciboLuzAbs(ABCMeta):
+
+    # Método estático
+    @staticmethod
+    def calcularTotalConsumo(lecturaActual, lecturaAnterior, costoKW):
+        consumoKw = lecturaActual - lecturaAnterior
+        costoConsumo = consumoKw * costoKW
+        if consumoKw < 500:
+            totalAPagar = costoConsumo * 1.22
+        elif consumoKw >= 500 and consumoKw < 900:
+            totalAPagar = costoConsumo * 1.18
+        else:
+            totalAPagar = costoConsumo * 1.12
+        totalAPagar = round(totalAPagar,2)
+        return totalAPagar
+
 class pruebaArchivos:
 
     def leerArchivo(self, archivo):
         file = open(archivo, 'r')
         lineas = []
+        lineas_archivo = []
         for linea in file.readlines():
-            lineas.append(linea)
+            lineas.append(linea.replace('\n', '').split(";"))
         file.close()
-        return lineas
+        for f in range(0, len(lineas)):
+            try:
+                lineas_archivo.append([int(lineas[f][0]), int(lineas[f][1]), float(lineas[f][2])])
+            except ValueError:
+                lineas_archivo.append([0,0,0.0])
+        return lineas_archivo
 
 if __name__ == "__main__":
     prueba = pruebaArchivos()
-    print(prueba.leerArchivo("retos.txt"))
+    archivo = []
+    archivo = prueba.leerArchivo("datos.txt")
+    print(archivo)
+    print(len(archivo))
