f2e89a1c9596d4420a17b40d3cf4fbefacfa9f67
Leonel Gonzalez Vidales <leonel@outlook.com>
2022-06-01 07:53:53 -0500

    Escritura de resultados en un archivo de texto

--- Archivos/pruebaArchivos.py
+++ Archivos/pruebaArchivos.py
@@ -2,7 +2,7 @@ from abc import ABCMeta
 
 class ReciboLuzAbs(ABCMeta):
 
-    # Método estático
+    # Método de clase
     @staticmethod
     def calcularTotalConsumo(lecturaActual, lecturaAnterior, costoKW):
         consumoKw = lecturaActual - lecturaAnterior
@@ -32,9 +32,29 @@ class pruebaArchivos:
                 lineas_archivo.append([0,0,0.0])
         return lineas_archivo
 
+    def calcularCobrosRecibo(self, lista):
+        resultados = []
+        for f in range(0, len(lista)):
+            resultados.append(ReciboLuzAbs.calcularTotalConsumo(
+                lista[f][0],lista[f][1],lista[f][2]))
+        return resultados
+
+    def guardarResultados(self, entrada, resultados):
+        file = open("resultados.txt", 'w')
+        file.write('Lectura Actual#Lectura Anterior#Costo Kw#Total a pagar\n')
+        for f in range(0, len(entrada)):
+            linea = str(entrada[f][0]) + '#' + \
+                    str(entrada[f][1]) + '#' + str(entrada[f][2]) + \
+                    '#' + str(resultados[f]) +'\n'
+            file.write(linea)
+        file.close()
+
 if __name__ == "__main__":
     prueba = pruebaArchivos()
     archivo = []
     archivo = prueba.leerArchivo("datos.txt")
     print(archivo)
-    print(len(archivo))
+    resultado = prueba.calcularCobrosRecibo(archivo)
+    print(resultado)
+    prueba.guardarResultados(archivo, resultado)
+
